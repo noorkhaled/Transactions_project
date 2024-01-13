ce695ddee2e045c227c82b824b472e992fcaa202
“Noorkhaled” <noorkhaled935@gmail.com>
2024-01-13 21:42:24 +0200

    Update:User_model

--- app/Models/User.php
+++ app/Models/User.php
@@ -12,34 +12,33 @@ class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
     protected $fillable = [
+        //user name
         'name',
+        //user email
         'email',
+        //user email`s password
         'password',
+        //user account_id
+        'account_id',
+        //user account_type
+        'account_type',
+        //user account balance
+        'balance'
     ];
-
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
     protected $hidden = [
         'password',
         'remember_token',
     ];
-
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array<string, string>
-     */
     protected $casts = [
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+    protected $table = 'users';
+
+    //relation between user and transaction that user can create many transactions
+    public function transactions()
+    {
+        return $this->hasMany(Transactions::class);
+    }
 }
