c8fc7fd231f360fe8ac2822926f6378972daae73
“Noorkhaled” <noorkhaled935@gmail.com>
2024-01-14 13:48:41 +0200

    Update:TransactionController

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -2,9 +2,66 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\TransactionRequest;
+use App\Models\Transactions;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class TransactionController extends Controller
 {
-    //
+    //Index function is used here to retrieve all transactions in DB
+    public function index()
+    {
+        $transactions = Transactions::all();
+        if (!$transactions) {
+            return response()->json([
+                'success' => false,
+                'message' => "no transactions found"
+            ]);
+        }
+        return response()->json([
+            'success' => true,
+            'transactions' => $transactions
+        ], 201);
+    }
+
+    //store function is to create new transaction
+    public function store(TransactionRequest $transactionRequest)
+    {
+        if (!$data = $transactionRequest->validated()) {
+            return response()->json([
+                'success' => false,
+                'message' => 'cannot create transaction',
+            ]);
+        }
+
+        $fromableUser = User::where('account_id', $data['fromable_account_id'])->first();
+        $toableUser = User::where('account_id', $data['toable_account_id'])->first();
+
+        $data['fromable_account_type'] = $fromableUser->account_type;
+        $data['toable_account_type'] = $toableUser->account_type;
+        $transactions = Transactions::create($data);
+        return response()->json([
+            'success' => true,
+            'message' => 'Transaction created successfully',
+            'transaction' => $transactions
+        ], 201);
+    }
+
+    //show function is used to retrieve a specific transaction by it`s ID
+    public function show($id)
+    {
+        $transaction = Transactions::find($id);
+        if (!$transaction) {
+            return response()->json([
+                'success' => false,
+                'message' => "Cannot locate Transaction with ID: '$id'"
+            ]);
+        }
+        return response()->json([
+            'success' => true,
+            'transaction' => $transaction
+        ], 201);
+    }
+
 }
