5cd7cfeb5af9079302dc4ee6b070f62ade45dc9d
“Noorkhaled” <noorkhaled935@gmail.com>
2024-01-14 13:28:47 +0200

    Update:OrderController

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -2,9 +2,50 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Orders;
 use Illuminate\Http\Request;
 
 class OrderController extends Controller
 {
-    //
+    public function index()
+    {
+        $orders = Orders::all();
+        return response($orders, 201);
+    }
+
+    public function store(Request $request)
+    {
+        $order = Orders::create($request->all());
+        return response()->json($order, 201);
+    }
+
+    public function show($id)
+    {
+        $order = Orders::find($id);
+        if (!$order) {
+            return response()->json([
+                'success' => false,
+                'message' => "Cannot locate order with ID: '$id'"
+            ]);
+        }
+        return response()->json([
+            'success' => true,
+            'order' => $order
+        ], 201);
+    }
+    public function delete($id)
+    {
+        $order = Orders::find($id);
+        if (!$order) {
+            return response()->json([
+                'success' => false,
+                'message' => "cannot find order with id: '$id'"
+            ], 201);
+        }
+        $order->delete();
+        return response()->json([
+            'success' => true,
+            'message' => "order with id: '$id' deleted",
+        ], 201);
+    }
 }
