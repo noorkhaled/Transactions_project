e280c1efa3754a1a93354fbca9ac5852ac431764
“Noorkhaled” <noorkhaled935@gmail.com>
2024-01-14 13:52:27 +0200

    Update:routes/api

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\OrderController;
+use App\Http\Controllers\TransactionController;
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +20,10 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::apiResource('users', UserController::class);
+Route::delete('users/{user}', [UserController::class, 'delete']);
+Route::put('users/{user}', [UserController::class, 'update']);
+
+Route::apiResource('transactions', TransactionController::class);
+
+Route::apiResource('orders', OrderController::class);
